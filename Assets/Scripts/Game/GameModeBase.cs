c110aceb25fe24e2734e3cf030459f5c8eeca35b
Benjamin <benni@barzweb.de>
2017-01-21 18:50:23 +0100

    init

---
+++ Assets/Scripts/Game/GameModeBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModeBase : MonoBehaviour
+{
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+    }
+
+
+    public void ConnectPlayer()
+    {
+
+    }
+
+    public void StartGame()
+    { 
+
+    }
+
+    public void EndGame()
+    {
+        
+    }
+
+
+    /* NETWORKING */
+    public void OnPlayerConnected(NetworkPlayer player)
+    {
+
+    }
+
+    public void OnPlayerDisconnected(NetworkPlayer player)
+    {
+
+    }
+
+}
